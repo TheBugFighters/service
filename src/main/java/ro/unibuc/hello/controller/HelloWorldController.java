48a31c3137942c99a26f5f79d6df6b2c04ae8d88
Raluca Pascu <ralucaioana2000@gmail.com>
2023-05-04 12:24:06 +0300

    [MONITORING] added monitoring + custom metrics for products enpdpoints

--- src/main/java/ro/unibuc/hello/controller/HelloWorldController.java
+++ src/main/java/ro/unibuc/hello/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +14,32 @@ import ro.unibuc.hello.dto.Greeting;
 import ro.unibuc.hello.exception.EntityNotFoundException;
 import ro.unibuc.hello.service.HelloWorldService;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloWorldController {
 
     @Autowired
     private HelloWorldService helloWorldService;
 
+    @Autowired
+    MeterRegistry metricsRegistry;
+
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/hello-world")
+    @Timed(value = "hello.greeting.time", description = "Time taken to return greeting")
+    @Counted(value = "hello.greeting.count", description = "Times greeting was returned")
     @ResponseBody
     public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        metricsRegistry.counter("my_non_aop_metric", "endpoint", "hello").increment(counter.incrementAndGet());
         return helloWorldService.hello(name);
     }
 
     @GetMapping("/info")
     @ResponseBody
+    @Timed(value = "hello.info.time", description = "Time taken to return info")
+    @Counted(value = "hello.info.count", description = "Times info was returned")
     public Greeting info(@RequestParam(name="title", required=false, defaultValue="Overview") String title) throws EntityNotFoundException {
         return helloWorldService.buildGreetingFromInfo(title);
     }
